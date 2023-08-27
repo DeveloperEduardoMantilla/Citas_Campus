b97baf95857c00829ab0cc6320bb0b645e1653c2
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-08-27 17:39:52 -0500

    feat(Citas-MongoDb): :sparkles: Get - Citas - Dia
    Se desarrolla EndPoint que permite obtener todas las citas para un día específico.
    
    EduardoMantillaCampus

--- resources/routes/citas.js
+++ resources/routes/citas.js
@@ -19,4 +19,29 @@ appCita.get("/", appVerifyCita, async(req, res)=>{
     res.send(result);
 })
 
+appCita.get("/:date", appVerifyCita, async(req, res)=>{
+    
+    let date = req.params.date;
+ 
+    let dateFormat= /^\d{4}-\d{2}-\d{2}$/;
+    
+    if (!dateFormat.test(date)) {
+        res.status(400).json({message:"Formato de fecha inválido. Utiliza aaaa-mm-dd."});
+        return;
+    }
+
+    let db = await conx();
+    let cita = db.collection("cita");
+
+    let result = await cita.find({cit_fecha:date},{projection:{
+        "_id":0,
+        "cita_codigo":"$cit_codigo",
+        "cita_fecha":"$cit_fecha",
+        "cita_estadoCita":"$cit_estadoCita",
+        "cita_medico":"$cit_medico",
+        "cita_datosUsuario":"$cit_datosUsuario",
+    }}).sort({cita_codigo:1}).toArray();
+    res.send(result);
+})
+
 export default appCita;
