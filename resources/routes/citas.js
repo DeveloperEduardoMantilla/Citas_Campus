ee79be59326bd41e11fdce7ff13bfbaa393b6d69
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-08-27 23:00:50 -0500

    feat(Citas-MongoDb): :sparkles: Get - Citas - Rechazadas - X mes
    Se desarrollo EndPoint que permite mostrar todas las citas que fueron rechazadas y en un mes específico, mostrar la fecha de la cita, el nombre del usuario y el médico.
    
    EduardoMantillaCampus

--- resources/routes/citas.js
+++ resources/routes/citas.js
@@ -44,4 +44,70 @@ appCita.get("/:date", appVerifyCita, async(req, res)=>{
     res.send(result);
 })
 
+appCita.get("/rechazada/:mes", appVerifyCita, async(req, res)=>{
+
+    try{
+
+    let mes = req.params.mes; 
+    let monthFormat = /^(1[0-2]|[1-9])$/;
+    
+    if (!monthFormat.test("10")) {
+        res.status(400).json({message:"Formato de mes inválido. Utiliza solo numero positivos entre 1 y 12."});
+        return;
+    }
+
+    if(mes <1 || mes >12){
+        res.status(400).json({message:"Utiliza solo numero positivos entre 1 y 12."});
+        return;
+    }
+
+    let db = await conx();
+    let cita = db.collection("cita");
+
+    let result = await cita.aggregate([
+        {
+            $match: {
+                "cit_estadoCita": 2,
+                "cit_fecha": {
+                    $regex: `^2023-${mes}-`
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "usuario",
+                localField: "cit_datosUsuario",
+                foreignField: "usu_id",
+                as: "usuario"
+            }
+        },
+        {
+            $lookup: {
+                from: "medico",
+                localField: "cit_medico",
+                foreignField: "med_nroMatriculaProsional",
+                as: "medico"
+            }
+        },
+        {
+            $project: {
+                "_id": 0,
+                "cita_fecha":"$cit_fecha",
+                "cita_estado":"Rechazada",
+                "cita_paciente":{ $arrayElemAt: ["$usuario.usu_nombre", 0] },
+                "cita_medico": { $arrayElemAt: ["$medico.med_nombreCompleto", 0] }
+            }
+        }
+    ]).toArray();
+    if(result.length>0){
+        res.send(result);
+    }else{
+        res.send({message:"No se encontraron registros en el sistema."})
+    }
+    
+    }catch(e){
+        res.status(400).send({message:e.message})
+    }
+})
+
 export default appCita;
