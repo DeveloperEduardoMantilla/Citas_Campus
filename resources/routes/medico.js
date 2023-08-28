1ef9c4eb5f30ad9514207026cab078c60ab6203e
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-08-27 19:30:59 -0500

    feat(Citas-MongoDb): :sparkles: Get - Contar - Citas
    Se desarrollo EndPoint que permite contar el número de citas que un médico tiene en un día específico
    
    EduardoMantillaCampus

--- resources/routes/medico.js
+++ resources/routes/medico.js
@@ -40,4 +40,64 @@ appMedico.get("/:especialidad", appVerifyMedico, async(req, res)=>{
         res.status(200).send(consult); 
 })
 
+appMedico.get("/consulta/consultorio", appVerifyMedico, async(req, res)=>{
+
+    let db = await conx(); 
+    let collection = await db.collection("medico");
+
+    let consult = await collection.aggregate([
+        {
+            $lookup: {
+              from: "consultorio",
+              localField: "med_consultorio",
+              foreignField: "cons_codigo",
+              as: "consultorio",
+            },
+        },
+        {
+            $project:{
+                "medico_numeroMatricula":"$med_nroMatriculaProsional",
+                "medico_nombreCompleto":"$med_nombreCompleto",
+                "medico_consultorio":{ $arrayElemAt: ["$consultorio.cons_nombre", 0]},
+                "medico_especialidad":"$med_especialidad",
+            }
+        }
+    ]).toArray();
+
+    res.status(200).send(consult); 
+})
+
+appMedico.get("/consulta/2/:medico/:fecha", appVerifyMedico, async (req, res)=>{
+    console.log("Entre aca");
+    let {medico, fecha} = req.params;
+
+    let dateFormat= /^\d{4}-\d{2}-\d{2}$/;
+    let medicoNumeroMat=/^[1-9][0-9]*$/;
+
+    if (!dateFormat.test(fecha)) {
+        res.status(400).json({message:"Formato de fecha inválido. Utiliza aaaa-mm-dd."});
+        return;
+    }
+    if (!medicoNumeroMat.test(medico)) {
+        res.status(400).json({message:"Formato de numero matricula medico inválido, solo se permite digitos positivos."});
+        return;
+    }
+
+    let db = await conx(); 
+    let collection = await db.collection("cita");
+    let consult = await collection.aggregate([{
+        $match: {
+            "cit_medico": parseInt(medico),
+            "cit_fecha": fecha
+        }
+    },{
+        $group: {
+            _id: "$cit_medico",
+            totalCitas: { $sum: 1 }
+        }
+    }
+    ]).toArray();
+
+    res.status(200).send(consult); 
+})
 export default appMedico;
